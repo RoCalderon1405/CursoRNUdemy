c26258173eda8d79d7e83fe349c6f3823005029f
RoCalderon1405 <rocalderon1405@hotmail.com>
2024-02-29 12:21:33 -0600

    Fin reforzamiento de React

--- 01-react-foundation/src/components/FormPage.tsx
+++ 01-react-foundation/src/components/FormPage.tsx
@@ -4,22 +4,31 @@ type FormInputs = {
   email: string;
   password: string;
 };
+
 export const FormPage = () => {
-  const {} = useForm<FormInputs>({
+  const { register, handleSubmit } = useForm<FormInputs>({
     defaultValues: {
       email: "roberto@email.com",
       password: "abc123",
     },
   });
 
+  const onSubmit = (myForm: FormInputs) => {
+    console.log(myForm);
+  };
+
   return (
     <>
       <h3>Formularios</h3>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Password" />
-          <button>Ingresar</button>
+          <input
+            type="text"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          <input type="text" placeholder="Password" {...register("password")} />
+          <button type="submit">Ingresar</button>
         </div>
       </form>
     </>
