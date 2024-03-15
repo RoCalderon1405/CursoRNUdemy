139b052ac60b12ab3f5a73d4b4d84e90db2a82e7
RoCalderon <rocalderon1405@hotmail.com>
2024-03-15 00:43:06 -0600

    Switches

--- 07-ComponentsApp/src/presentation/screens/switches/SwitchScreen.tsx
+++ 07-ComponentsApp/src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import {Switch, Text, View} from 'react-native';
+import {globalStyles} from '../../../config/theme/theme';
+import {CustomView} from '../../components/ui/CustomView';
+import {Button} from '../../components/ui/Button';
+import {Card} from '../../components/ui/Card';
+import {useState} from 'react';
 
 export const SwitchScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <CustomView style={{}}>
+      <Card>
+        <Switch
+          trackColor={{false: '#767577', true: '#81b0ff'}}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </Card>
+    </CustomView>
   );
 };
